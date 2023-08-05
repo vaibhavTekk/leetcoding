0d2220df2e1f6b690171dc8fec73ba70432e156b
Vaibhav Tekkalur <59783171+vaibhavTekk@users.noreply.github.com>
2023-08-05 17:06:48 +0000

    Sync LeetCode submission - N-ary Tree Preorder Traversal (python3)

---
+++ problems/n-ary_tree_preorder_traversal/solution.py
@@ -0,0 +1,17 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        return self.traverse(root,[])
+    def traverse(self,root,arr):
+        if root:
+            arr.append(root.val)
+            for i in root.children:
+                self.traverse(i,arr)
+        return arr
