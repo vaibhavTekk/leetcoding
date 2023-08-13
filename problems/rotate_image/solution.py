80dcbf77196ea838bdb4935cd80198d668d19dda
Vaibhav Tekkalur <59783171+vaibhavTekk@users.noreply.github.com>
2023-08-13 06:12:32 +0000

    Sync LeetCode submission - Rotate Image (python3)

---
+++ problems/rotate_image/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        for i in range(len(matrix)):
+            for j in range(i,len(matrix)):
+                if i != j:
+                    temp = matrix[i][j]
+                    matrix[i][j] = matrix[j][i]
+                    matrix[j][i] = temp
+
+        for i in matrix:
+            i.reverse()
